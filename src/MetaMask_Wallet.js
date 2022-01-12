456a5a01ac45879e9cdea0f23fccf92e61464189
jmayanja-xyz <51414069+jmayanja-xyz@users.noreply.github.com>
2022-01-12 21:57:42 +0000

    adding candidate functionality to smart contract

--- src/MetaMask_Wallet.js
+++ src/MetaMask_Wallet.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ethers } from 'ethers';
+import React, { useState, useEffect } from 'react';
+import { ethers, Contract } from 'ethers';
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table';
 import { Container, Form } from 'react-bootstrap';
@@ -7,80 +7,66 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Ballot from './contracts/Ballot.json';
 
 
-
-
 const MetaMask_Wallet = () => {
-
-//let state = { hasVoted: 0, web3: null, accounts: null, contract: null };
-
 
 const [errorMessage, setErrorMessage] = useState(null);
 const [defaultAccount, setDefaultAccount] = useState(null);
 const [userBalance, setUserBalance] = useState(null);
 const [connectButtonText, setConnectButtonText] = useState('Connect Wallet');
 
-async function castVote () {
+const [data, setData] = useState(undefined);
+
+let contractAddress;
+
+
+async function getVote() {
+    let add = "0xC0c8F83e7f3Fb1Db81778587a8b0ae1D35Fa005D";
+    const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner()
+    let factory = new ethers.Contract(add, Ballot.abi, signer);
+
+    try {
+        const data = await factory.getCandidatesCount()
+        console.log(data.toString());
+    } catch (err) {
+        console.log("Error: ", err);
+    }   
+}
+
+
+
+
+
+async function addCanididate() {
     try {
         console.log('Running deployWithEthers script...')
 
         const currentAccount = defaultAccount; 
 
-        const candidateId = 0;
-    
-        const constructorArgs = []    // Put constructor args (if any) here for your contract
-
-        // Note that the script needs the ABI which is generated from the compilation artifact.
-        // Make sure contract is compiled and artifacts are generated
-
-        console.log(Ballot.abi)
-    
-        // const metadata = JSON.parse(Ballot.abi
-        //     )
-        // console.log(metadata);
-        console.log(Ballot);
+        console.log(contractAddress);
+        let add = "0xC0c8F83e7f3Fb1Db81778587a8b0ae1D35Fa005D";
 
 
         const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner()
-    
-        let factory = new ethers.Contract(currentAccount, Ballot.abi, signer);
-        console.log(factory);
 
-        console.log(factory.candidates(0));
+        console.log(add);
+        let factory = new ethers.Contract(add, Ballot.abi, signer);
 
-    
-        console.log('Contract Address: ', factory.address);
-        await factory.vote(candidateId, { from: currentAccount })
-
-       
+        const tx = await factory.addCandidate("Aguero");
+        const receipt = await tx.wait();
+        console.log(receipt);
         
-
-        // The contract is NOT deployed yet; we must wait until it is mined
+        console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+        console.log(`Gas used: ${receipt.gasUsed.toString()}`);
   
         accountChangedHandler(currentAccount);
-        console.log('Deployment successful.')
+        console.log('Deployment successful.');
+        getVote()
     } catch (e) {
         console.log(e.message)
     }
-}             
+}        
 
-// it("allows a voter to cast a vote", function() {
-//     return Election.deployed().then(function(instance) {
-//       electionInstance = instance;
-//       candidateId = 1;
-//       return electionInstance.vote(candidateId, { from: accounts[0] });
-//     }).then(function(receipt) {
-//       assert.equal(receipt.logs.length, 1, "an event was triggered");
-//       assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
-//       assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
-//       return electionInstance.voters(accounts[0]);
-//     }).then(function(voted) {
-//       assert(voted, "the voter was marked as voted");
-//       return electionInstance.candidates(candidateId);
-//     }).then(function(candidate) {
-//       var voteCount = candidate[2];
-//       assert.equal(voteCount, 1, "increments the candidate's vote count");
-//     })
-//   });
+
 
 
 async function deployContract () {
@@ -88,19 +74,29 @@ async function deployContract () {
         console.log('Running deployWithEthers script...')
 
         let currentAccount = defaultAccount; 
+
     
-        const constructorArgs = []    // Put constructor args (if any) here for your contract
+        const constructorArgs = [
+        
+        ]    // Put constructor args (if any) here for your contract
 
         // Note that the script needs the ABI which is generated from the compilation artifact.
         // Make sure contract is compiled and artifacts are generated
 
-        console.log(Ballot.abi)
+        console.log(Ballot.abi);
+        
+        const FormatTypes = ethers.utils.FormatTypes;
+
+        const iface = new ethers.utils.Interface(Ballot.abi);
+        console.log(iface);
+        iface.format(FormatTypes.full);
+
+        console.log(iface);
+       // check this out
+        console.log(iface._abiCoder);
+
+
     
-        // const metadata = JSON.parse(Ballot.abi
-        //     )
-        // console.log(metadata);
-        console.log(Ballot);
-        console.log(Ballot.deployedBytecode);
 
         const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner()
     
@@ -109,16 +105,15 @@ async function deployContract () {
         let contract = await factory.deploy(...constructorArgs);
     
         // The contract is NOT deployed yet; we must wait until it is mined
-        await contract.deployed()
+        const tx = await contract.deployed()
+        contractAddress = tx.address;
+        console.log(contractAddress);
         accountChangedHandler(currentAccount);
         console.log('Deployment successful.')
     } catch (e) {
         console.log(e.message)
     }
 }       
-
-
-
 
 
 
@@ -171,14 +166,6 @@ window.ethereum.on('chainChanged', chainChangedHandler);
 
 
 
-
-
-
-
-
-
-
-
     return (
 		<div className='Wallet'>
             <br />  
@@ -212,7 +199,7 @@ window.ethereum.on('chainChanged', chainChangedHandler);
                             <tr>
                             <td>1</td>
                             <td></td>
-                            <td></td>
+                            <td>{data}</td>
                             </tr>
 
 
@@ -221,29 +208,34 @@ window.ethereum.on('chainChanged', chainChangedHandler);
                             <td>2</td>
                             <td></td>
                             <td></td>
-
                             </tr>
                         </tbody>
                     </Table>
                 </div>
-                <br />  
+                <br/>  
 
                 <div>
                     <Form.Select>
                         <option></option>
                     </Form.Select>
                 </div>
-                <br />  
+                <br/>  
+
+                <div>
+                    <Button onClick={deployContract} variant="secondary">Deploy Contract</Button>{' '}
+                </div>
+
+                <br/> 
 
 
 
                 <div>
-                    <Button onClick={castVote} variant="success">Vote</Button>{' '}
+                    <Button onClick={addCanididate} variant="warning">Add Candidate</Button>{' '}
                 </div>
                 
                 {errorMessage}
                 </Container> 
-                <br />  
+                <br/>  
 		</div>
 	);
 }
